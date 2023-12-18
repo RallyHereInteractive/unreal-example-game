46a1ca0216d7e75d4f3677584e3d6257c2eb9a81
cranney <cranney@rallyhere.gg>
2023-12-18 13:20:11 -0500

    Fix login error ftext format

--- Plugins/RallyHereStart/Source/RallyHereStart/Private/DataFactories/RHLoginDataFactory.cpp
+++ Plugins/RallyHereStart/Source/RallyHereStart/Private/DataFactories/RHLoginDataFactory.cpp
@@ -419,7 +419,7 @@ void URHLoginDataFactory::OnRHLoginResponse(const FRH_LoginResult& LR)
         LoginEvent_ShowAgreements(LR.bMustAcceptEULA, LR.bMustAcceptTOS, LR.bMustAcceptPP);
         break;
     case ERHAPI_LoginResult::Fail_RHDenied:
-		LoginEvent_FailedClient(FText::Format(NSLOCTEXT("RHErrorMsg", "AccessDenied", "Access Denied ({0})"), *LR.RallyHereErrorCode));
+		LoginEvent_FailedClient(FText::Format(NSLOCTEXT("RHErrorMsg", "AccessDenied", "Access Denied ({0})"), FText::FromString(LR.RallyHereErrorCode)));
         break;
     case ERHAPI_LoginResult::Fail_RHUnknown:
 		LoginEvent_FailedClient(NSLOCTEXT("RHErrorMsg", "UnknownError", "Unknown Error"));
